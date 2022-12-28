43fe81a82ac46e9e2043eb9239b62770bc2343f2
Lukas <ho_lu@gmx.de>
2022-12-28 14:48:57 +0100

    Game Over Screen

--- GameOver.gd
+++ GameOver.gd
@@ -12,8 +12,30 @@ func _ready():
 	yield(get_tree().create_timer(1.2), "timeout")
 	$TextureRect/AnimatedSprite.play("gameover")
 
+<<<<<<< Updated upstream
 func _on_AnimatedSprite_animation_finished():
 	yield(get_tree().create_timer(3), "timeout")
 	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	SavedData.reset_data()
 	SceneTransistor.start_transition_to("res://Game.tscn")
+=======
+func _input(event: InputEvent) -> void:
+	if animation_finished == true:
+		if event.is_action_pressed("ui_down"):
+			self.current_option += 1
+		elif event.is_action_pressed("ui_up"):
+			self.current_option -= 1
+		if event.is_action_pressed("ui_accept"):
+			match current_option:
+				CONTINUE:
+					Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+					SavedData.reset_data()
+					SceneTransistor.start_transition_to("res://Game.tscn")
+				EXIT:
+					get_tree().quit()
+
+func set_current_option(new_option: int) -> void:
+	current_option = clamp(new_option, 0, $VBoxContainer.get_child_count()-1)
+	$Sprite.position = $VBoxContainer.get_child(current_option).rect_global_position + Vector2(-10,
+											$VBoxContainer.get_child(current_option).rect_size.y/2)
+>>>>>>> Stashed changes
